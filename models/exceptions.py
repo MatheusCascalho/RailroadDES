2e026b19fae1e5f96adc2319d788f3ca0d701b59
matheus_cascalho <cascalhom@gmail.com>
2022-03-22 09:28:17 -0300

    exceções

--- models/exceptions.py
+++ models/exceptions.py
@@ -2,3 +2,7 @@ class TrainExceptions(Exception):
     @staticmethod
     def path_is_finished():
         raise TrainExceptions('Path is finished!')
+
+    @staticmethod
+    def volume_to_unload_is_greater_than_current_volume():
+        raise TrainExceptions('Volume to unload is greater than current volume!')
