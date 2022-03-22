8b129cdb1429c7f5c72ffe12041a172799077f0c
matheus_cascalho <cascalhom@gmail.com>
2022-03-22 09:28:08 -0300

    objetos de condição do sistema

---
+++ models/conditions.py
@@ -0,0 +1,36 @@
+import abc
+from des_simulator import DESSimulator
+import model_queue as mq
+from dataclasses import dataclass, field, InitVar
+from typing import Any, Generator, Callable
+from datetime import timedelta, datetime
+from entities import Node, Train
+from event_calendar import Event
+
+
+@dataclass
+class TransitTime:
+    load_origin: int
+    load_destination: int
+    loaded_time: timedelta
+    empty_time: timedelta
+
+
+@dataclass
+class RailroadMesh:
+    load_points: tuple[Node]
+    unload_points: tuple[Node]
+    transit_times: list[TransitTime]
+
+    def __iter__(self):
+        all_points = self.load_points + self.unload_points
+        return all_points.__iter__()
+
+    def transit_time(self, origin, destination):
+        is_loaded_transit = origin in self.load_points
+        for transit in self.transit_times:
+            if is_loaded_transit and transit.load_origin == origin and transit.load_destination == destination:
+                return transit.loaded_time
+            elif not is_loaded_transit and transit.load_origin == destination and transit.load_destination == origin:
+                return transit.empty_time
+
