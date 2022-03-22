beb6b196e6d08481bbbee638027741b2f04bb647
matheus_cascalho <cascalhom@gmail.com>
2022-03-22 09:28:40 -0300

    constantes

--- models/constants.py
+++ models/constants.py
@@ -1,1 +1,15 @@
+from enum import IntEnum
+
+
 EPSILON = 1e-1
+
+
+class TrainActions:
+    MOVING = 0
+    LOADING = 1
+    UNLOADING = 2
+    MANEUVERING_TO_ENTER = 3
+    MANEUVERING_TO_LEAVE = 4
+    WAITING_ON_QUEUE_TO_ENTER = 5
+    WAITING_ON_QUEUE_TO_LEAVE = 6
+
