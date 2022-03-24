1154bd1af84141698a1d191246a25f4f2b2a3145
matheus_cascalho <cascalhom@gmail.com>
2022-03-23 23:10:06 -0300

    inicialização de identificador dos nós

--- models/conditions.py
+++ models/conditions.py
@@ -21,15 +21,26 @@ class RailroadMesh:
     unload_points: tuple[NodeInterface]
     transit_times: list[TransitTime]
 
+    def __post_init__(self):
+        self.name_to_id = {}
+        for i, node in enumerate(self):
+            node.identifier = i
+            self.name_to_id[node.name] = node.identifier
+
     def __iter__(self):
         all_points = self.load_points + self.unload_points
         return all_points.__iter__()
 
-    def transit_time(self, origin, destination):
-        is_loaded_transit = origin in self.load_points
+    def transit_time(self, origin_id, destination_id):
+        is_loaded_transit = origin_id in [node.identifier for node in self.load_points]
         for transit in self.transit_times:
-            if is_loaded_transit and transit.load_origin == origin and transit.load_destination == destination:
+            if (
+                    is_loaded_transit and
+                    self.name_to_id[transit.load_origin] == origin_id and
+                    self.name_to_id[transit.load_destination] == destination_id
+            ):
                 return transit.loaded_time
-            elif not is_loaded_transit and transit.load_origin == destination and transit.load_destination == origin:
+            elif not is_loaded_transit and transit.load_origin == destination_id and transit.load_destination == origin_id:
                 return transit.empty_time
+        return timedelta(days=float('inf'))
 
