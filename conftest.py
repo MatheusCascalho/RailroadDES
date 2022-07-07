62271c80b000b6514f90fcbdf6dad2cd1464e834
matheus cascalho <matheus.santos@enacom.com.br>
2022-07-07 07:28:15 -0300

    simple net fixture

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
 from artificacts.des_simulator import fake_des_simulator
 from artificacts.node import fake_node
 from artificacts.train import empty_train
+from artificacts.petri_nets import simple_net
