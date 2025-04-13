d5883897ba85ac0e79f0f8b88e492f02fe6201bd
Matheus Cascalho <cascalhom@gmail.com>
2025-04-13 18:17:25 -0300

    máquinas de estado para controle do trem

--- models/train.py
+++ models/train.py
@@ -12,39 +12,202 @@ from datetime import timedelta, datetime
 from models.exceptions import TrainExceptions, FinishedTravelException
 from models.states import (
     TrainState,
-    TimeRegister,
-    NodeState
+    NodeState,
+    LoadState,
+    ActivityState
 )
+from models.time_table import TimeRegister
 from models.resources import Slot
 from models.entities import Entity
 from interfaces.train_interface import TrainInterface
 from interfaces.node_interce import NodeInterface
 from collections import defaultdict
 from models.demand import Demand
+from models.state_machine import StateMachine, Transition, State
+from models.task import Task
+from models.time_table import EventName, TimeEvent
 
 
-@dataclass
-class Train(TrainInterface):
-
-    id: int
-    origin: int
-    destination: int
-    model: int
-    path: list[int]
-    capacity: float
-    target_demand: Demand = field(init=False)
-    state: TrainState = field(init=False)
-    time_table: dict[int, list[TimeRegister]] = field(init=False, default_factory=lambda : defaultdict(list))
-    current_location: InitVar[Union[int, list[int]]]
-    initial_volume: InitVar[float] = field(default=0.0)
-
-    def __post_init__(self, current_location, initial_volume: float):
-        self.state = TrainState(
-            volume=initial_volume,
-            current_location=current_location,
-            action=TrainActions.WAITING_ON_QUEUE_TO_LEAVE if isinstance(current_location, int) else TrainActions.MOVING,
-            time_register=TimeRegister()
+class LoadSystem:
+    def __init__(
+            self,
+            capacity: float,
+            is_loaded: bool,
+            processing_state: State,
+            queue_to_leave_state: State
+    ):
+        self.capacity = capacity
+        self.state_machine = self.build_state_machine(
+            processing_state=processing_state,
+            queue_to_leave_state=queue_to_leave_state,
+            is_loaded=is_loaded
         )
+        self.volume = capacity if is_loaded else 0
+
+    def __str__(self):
+        return str(self.state_machine.current_state)
+
+    __repr__ = __str__
+    def build_state_machine(self, processing_state, queue_to_leave_state, is_loaded):
+        loaded = State(name=LoadState.LOADED, is_marked=is_loaded)
+        loading = State(name=LoadState.LOADING, is_marked=False)
+        empty = State(name=LoadState.EMPTY, is_marked=not is_loaded)
+        unloading = State(name=LoadState.UNLOADING, is_marked=False)
+        to_loaded = Transition(
+            name="to_loaded",
+            origin=loading,
+            destination=loaded,
+            action=self.load
+        )
+        to_unloading = Transition(
+            name="to_unloading",
+            origin=loaded,
+            destination=unloading
+        )
+        to_empty = Transition(
+            name="to_empty",
+            origin=unloading,
+            destination=empty,
+            action=self.unload
+        )
+        to_loading = Transition(
+            name="to_loading",
+            origin=empty,
+            destination=loading
+        )
+
+        processing_state.add_observer([to_loading, to_unloading])
+        queue_to_leave_state.add_observer([to_loaded, to_empty])
+        transitions = [to_loaded, to_empty, to_loading, to_unloading]
+        sm = StateMachine(transitions=transitions)
+        return sm
+
+    def set_processing_state(self, processing_state: State):
+        loading = self.state_machine.transitions['to_loading']
+        unloading = self.state_machine.transitions['to_unloading']
+        processing_state.add_observer([loading, unloading])
+
+    def set_in_queue_state(self, in_queue_state: State):
+        loaded = self.state_machine.transitions['to_loaded']
+        empty = self.state_machine.transitions['to_empty']
+        in_queue_state.add_observer([loaded, empty])
+
+    def load(self):
+        self.volume = self.capacity
+
+    def unload(self):
+        self.volume = 0
+
+class ActivitySystem:
+    def __init__(self, path):
+        self.state_machine = self.build_transitions()
+        self.__path = path
+        self.current_location = path[0]
+        print(self)
+
+    def __str__(self):
+        return str(self.state_machine.current_state)
+
+    __repr__ = __str__
+
+    @property
+    def path(self):
+        return self.__path
+
+    @path.setter
+    def path(self, new_path):
+        self.__path = new_path
+        self.current_location = self.__path[0]
+
+    @staticmethod
+    def build_transitions():
+        moving = State(name=ActivityState.MOVING, is_marked=False)
+        queue_to_enter = State(name=ActivityState.QUEUE_TO_ENTER, is_marked=True)
+        processing = State(name=ActivityState.PROCESSING, is_marked=False)
+        queue_to_leave = State(name=ActivityState.QUEUE_TO_LEAVE, is_marked=False)
+
+        leave = Transition(
+            name="leave",
+            origin=queue_to_leave,
+            destination=moving
+        )
+        arrive = Transition(
+            name="arrive",
+            origin=moving,
+            destination=queue_to_enter
+        )
+        start = Transition(
+            name="start",
+            origin=queue_to_enter,
+            destination=processing
+        )
+        finish = Transition(
+            name="finish",
+            origin=processing,
+            destination=queue_to_leave
+        )
+        transitions = [
+            leave, arrive,
+            start, finish
+        ]
+        sm = StateMachine(transitions=transitions)
+        return sm
+
+    @property
+    def processing_state(self):
+        return self.state_machine.states[ActivityState.PROCESSING]
+
+
+    @property
+    def queue_to_leave_state(self):
+        return self.state_machine.states[ActivityState.QUEUE_TO_LEAVE]
+
+
+    def finish_process(self):
+        if self.state_machine.current_state.name == ActivityState.PROCESSING:
+            self.state_machine.update()
+
+    def start_process(self):
+        if self.state_machine.current_state.name == ActivityState.QUEUE_TO_ENTER:
+            self.state_machine.update()
+
+    def leave(self):
+        if self.state_machine.current_state.name == ActivityState.QUEUE_TO_LEAVE:
+            self.state_machine.update()
+
+    def arrive(self):
+        if self.state_machine.current_state.name == ActivityState.MOVING:
+            self.state_machine.update()
+            self.path = self.path[1:]
+
+def train_id_gen():
+    i = 0
+    while True:
+        ID = f"train_{i}"
+        yield ID
+        i += 1
+
+train_id = train_id_gen()
+
+class Train(TrainInterface):
+    def __init__(
+            self,
+            capacity: float,
+            task: Task,
+            is_loaded: bool
+    ):
+        self.ID = next(train_id)
+
+        self.activity_system = ActivitySystem(
+            path=task.path
+        )
+        self.load_system = LoadSystem(
+            capacity=capacity,
+            is_loaded=is_loaded,
+            processing_state=self.activity_system.processing_state,
+            queue_to_leave_state=self.activity_system.queue_to_leave_state
+        )
+        self.__current_task = task
 
 
     # ====== Properties ==========
