78e75b5de3dc19c06c2c18bc58d35818a4064100
Matheus Cascalho <cascalhom@gmail.com>
2025-05-04 13:05:42 -0300

    atualização de fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
 from tests.artifacts.stock_node_artifacts import *
 from tests.artifacts.train_artifacts import *
 from tests.artifacts.rail_segment_artificacts import *
+from tests.artifacts.time_table_artifacts import *
+from tests.artifacts.node_artifacts import *
