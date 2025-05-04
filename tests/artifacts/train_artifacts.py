7c7872c1e3233abebc97b588876152225eb778b2
Matheus Cascalho <cascalhom@gmail.com>
2025-05-04 14:13:10 -0300

    artefatos de trem

--- tests/artifacts/train_artifacts.py
+++ tests/artifacts/train_artifacts.py
@@ -1,11 +1,17 @@
+from datetime import timedelta
+
 from pytest import fixture
 from models.demand import Flow, Demand
+from models.railroad import RailSegment
 from models.task import Task
 from models.train import Train
+from models.arrive_scheduler import ArriveScheduler
+from tests.test_integration import FakeSimulator
+
 
 @fixture
-def simple_train():
-    def make(product, clk, train_size=6e3):
+def simple_train(basic_node_factory):
+    def make(product, clk, simulator, train_size=6e3):
         flow = Flow(
             origin="origin",
             destination="destination",
@@ -15,11 +21,23 @@ def simple_train():
             flow=flow,
             volume=103e3
         )
+        segment1 = RailSegment(
+            origin=basic_node_factory("origin"),
+            destination=basic_node_factory("destination"),
+            time_to_origin=timedelta(hours=10),
+            time_to_destination=timedelta(hours=10)
+        )
+        segment2 = segment1.reversed()
+        scheduler = ArriveScheduler(
+            rail_segments=[segment1, segment2],
+            simulator=simulator
+        )
         task = Task(
             demand=demand,
             path=["origin", "destination"],
             task_volume=train_size,
-            current_time=clk.current_time
+            current_time=clk.current_time,
+            scheduler=scheduler
         )
         train = Train(
             capacity=train_size,
