aa8b93aa9519e87fc048be5f6cd586c263c2da66
Matheus Cascalho <cascalhom@gmail.com>
2025-05-04 13:04:35 -0300

    fixtures de time_table movidas para um arquivo próprio

---
+++ tests/artifacts/time_table_artifacts.py
@@ -0,0 +1,33 @@
+from models.time_table import (
+    TimeTable,
+    TimeEvent,
+    TimeRegister
+)
+from models.constants import Process, EventName
+from datetime import datetime
+import pytest
+
+@pytest.fixture
+def time_table():
+    """Fixture to initialize an empty TimeTable."""
+    return TimeTable()
+
+
+@pytest.fixture
+def time_register():
+    """Fixture to initialize a TimeRegister with example data."""
+    arrive = TimeEvent(
+        EventName.ARRIVE,
+        instant=datetime(2025, 4, 10, 12, 0)
+    )
+    start = TimeEvent(EventName.START_PROCESS, datetime(2025, 4, 10, 12, 15))
+    finish = TimeEvent(EventName.FINISH_PROCESS, datetime(2025, 4, 10, 12, 45))
+    departure = TimeEvent(EventName.DEPARTURE, datetime(2025, 4, 10, 13, 0))
+    time_register = TimeRegister(
+        process=Process.LOAD,
+        arrive=arrive,
+        start_process=start,
+        finish_process=finish,
+        departure=departure
+    )
+    return time_register
--- tests/test_time_table.py
+++ tests/test_time_table.py
@@ -9,30 +9,6 @@ from models.time_table import (
 from models.exceptions import EventSequenceError, TimeSequenceErro, RepeatedProcessError, AlreadyRegisteredError
 
 
-@pytest.fixture
-def time_table():
-    """Fixture to initialize an empty TimeTable."""
-    return TimeTable()
-
-
-@pytest.fixture
-def time_register():
-    """Fixture to initialize a TimeRegister with example data."""
-    arrive = TimeEvent(
-        EventName.ARRIVE,
-        instant=datetime(2025, 4, 10, 12, 0)
-    )
-    start = TimeEvent(EventName.START_PROCESS, datetime(2025, 4, 10, 12, 15))
-    finish = TimeEvent(EventName.FINISH_PROCESS, datetime(2025, 4, 10, 12, 45))
-    departure = TimeEvent(EventName.DEPARTURE, datetime(2025, 4, 10, 13, 0))
-    time_register = TimeRegister(
-        process=Process.LOAD,
-        arrive=arrive,
-        start_process=start,
-        finish_process=finish,
-        departure=departure
-    )
-    return time_register
 
 
 def test_time_table_initialization(time_table):
