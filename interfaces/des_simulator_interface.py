7131fc4007555f1d1cd87d0677a9d3a37cf4b57f
Matheus Cascalho <cascalhom@gmail.com>
2025-05-04 14:05:39 -0300

    adição de novos métodos na interface do DESSimulator

--- interfaces/des_simulator_interface.py
+++ interfaces/des_simulator_interface.py
@@ -1,9 +1,23 @@
+from abc import abstractmethod
+
+
 class DESSimulatorInterface:
     """
     Discrete Event System simulator
     """
+    @abstractmethod
     def add_event(self, time, callback, **data):
         pass
 
+    @abstractmethod
     def simulate(self, model, time_horizon=28 * 3600):
+        pass
+
+    @abstractmethod
+    def solve_exceptions(self, *args, **kwargs):
+        pass
+
+    @property
+    @abstractmethod
+    def current_date(self):
         pass
