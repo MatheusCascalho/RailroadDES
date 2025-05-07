cf2a2df32d73346a89b3c5746be8abcde3579484
Matheus Cascalho <cascalhom@gmail.com>
2025-05-06 22:08:50 -0300

    arrive_scheduler como observador do trem

--- models/arrive_scheduler.py
+++ models/arrive_scheduler.py
@@ -2,7 +2,6 @@ from interfaces.des_simulator_interface import DESSimulatorInterface
 from interfaces.train_interface import TrainInterface
 from models.railroad import RailSegment
 from models.observers import AbstractObserver
-from models.time_table import TimeTable
 from models.exceptions import FinishedTravelException
 
 
@@ -15,38 +14,33 @@ class ArriveScheduler(AbstractObserver):
     ):
         self.rail_segments = rail_segments
         self.simulator = simulator
-        self.train = train
         super().__init__()
 
-    @property
-    def train(self):
-        return self._train
-
-    @train.setter
-    def train(self, value):
-        self._train = value
-
-    def append_subject(self, sub):
-        if not isinstance(sub, TimeTable):
-            raise Exception("Scheduler only look for TimeTable Objects")
+    def append_subject(self, sub, update=False):
+        if len(self.subjects)>0:
+            raise Exception("Scheduler is already looking to a train")
+        if not isinstance(sub, TrainInterface):
+            raise Exception("Scheduler only look for Train Objects")
         self.subjects.append(sub)
-        self.update()
+        if update:
+            self.update()
 
     def send(self):
         try:
             current_segment = self.current_segment()
-        except FinishedTravelException:
-            self.simulator.solve_exceptions()
+        except FinishedTravelException as error:
+            self.simulator.solve_exceptions(train=self.subjects[0], error=error)
             current_segment = self.current_segment()
 
         self.simulator.add_event(
             time=current_segment.time_to_destination,
             callback=current_segment.destination.receive,
-            train=self.train
+            train=self.subjects[0],
+            simulator=self.simulator
         )
 
     def update(self):
-        if self.train and self.subjects[0].dispatched_just_now:
+        if self.subjects[0].dispatched_just_now:
             self.send()
         if self.subjects[0].arrived_right_now:
             self.rail_segments = self.rail_segments[1:]
@@ -54,7 +48,7 @@ class ArriveScheduler(AbstractObserver):
     def current_segment(self):
         if not self.rail_segments:
             FinishedTravelException.path_is_finished(
-                train=self.train,
+                train=self.subjects[0],
                 current_time=self.simulator.current_date
             )
         return self.rail_segments[0]
