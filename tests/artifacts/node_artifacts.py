329048afbbfe3139ca9b1785a4bcb883952d2c14
Matheus Cascalho <cascalhom@gmail.com>
2025-05-13 20:22:25 -0300

    Add factory classes and data generation for test scaffolding
    Introduce `StockFactory`, `RateDataFactory`, `ProcessorRateFactory`, and `StockNodeDataFactory` to generate mock data for testing. Enhance data initialization with localized Faker and streamline test preparation using factories, transit time parsing, and mesh creation.

--- tests/artifacts/node_artifacts.py
+++ tests/artifacts/node_artifacts.py
@@ -1,7 +1,18 @@
+import json
+
 import pytest
 from models.model_queue import Queue
 from models.constants import Process
 from models.node import Node
+from faker import Faker
+from factory import Factory, LazyAttribute
+from models.node_data_model import StockData, ProcessorRate, StockNodeData, RateData
+from faker_food import FoodProvider
+from models.railroad_mesh import RailroadMesh
+from models.node_factory import NodeStockFactory
+from models.clock import Clock
+from datetime import datetime, timedelta
+from models.railroad_mesh import TransitTime
 
 
 @pytest.fixture
@@ -75,3 +86,82 @@ def real_node_factory(mocker):
             process_units=[]
         )
     return make
+
+
+fake = Faker('pt_BR')  # Definindo o locale para português do Brasil
+fake.add_provider(FoodProvider)
+
+class StockFactory(Factory):
+    class Meta:
+        model = StockData
+
+    product = LazyAttribute(lambda x: fake.vegetable())
+    capacity = LazyAttribute(
+        lambda x: max(fake.random_number(digits=5),10_000)#, 10.00)  # Desconto mínimo de 10.00
+    )
+    initial_volume = LazyAttribute(
+        lambda obj: min(fake.random_number(digits=5),obj.capacity)
+    )
+
+
+class RateDataFactory(Factory):
+    class Meta:
+        model = RateData
+
+    product: str
+    rate = fake.random_number(digits=4)
+
+
+class ProcessorRateFactory(Factory):
+    class Meta:
+        model = ProcessorRate
+    product: str
+    type: Process
+    rate = fake.random_number(digits=4)
+
+class StockNodeDataFactory(Factory):
+    class Meta:
+        model = StockNodeData
+
+    name: str = "xpto"
+    stocks: list[StockData] = [StockFactory() for _ in range(5)]
+    rates: list[ProcessorRate] = []
+    replenishment: list[dict] = []
+    train_sizes: list[float] = []
+    queue_capacity: int = 20
+    post_operation_time: int = fake.random_number(digits=2)/10
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        process = kwargs.pop('process', None)  # Pega o parâmetro extra
+        obj = super()._create(model_class, *args, **kwargs)
+
+        # Agora você pode manipular o objeto ou fazer algo com o extra_param
+        cls.rates = [ProcessorRateFactory(product=stock.product, type=process) for stock in obj.stocks]
+        # if extra_param:
+        #     print(f"Valor extra recebido: {extra_param}")
+
+        return obj
+
+if __name__ == '__main__':
+    import json
+
+    with open('transit_times.json', 'r') as f:
+        transits = json.load(f)
+    nodes = []
+    load_points = []
+    unload_points = []
+    clk = Clock(start=datetime(2025, 4, 1), discretization=timedelta(hours=1))
+    for tt in transits:
+        origin = StockNodeDataFactory(name=tt['load_origin'], process=Process.LOAD)
+        destination = StockNodeDataFactory(name=tt['load_destination'], process=Process.UNLOAD)
+        load_points.append(NodeStockFactory(data=origin, clock=clk).create_node())
+        unload_points.append(NodeStockFactory(data=destination, clock=clk).create_node())
+    mesh = RailroadMesh(
+        load_points=tuple(load_points),
+        unload_points=tuple(unload_points),
+        transit_times=[TransitTime(**t) for t in transits]
+    )
+    print(mesh)
+
+
