7ff8e74987f30ca4b165d593827a219aed0d87a3
Matheus Cascalho <cascalhom@gmail.com>
2025-06-23 00:52:33 -0300

    configuração de logs no __init__

---
+++ models/config_log.py
@@ -0,0 +1,14 @@
+# config_log.py
+import logging
+
+def configurar_logs():
+    # Configuração básica de logging
+    logging.basicConfig(level=logging.CRITICAL,  # Define o nível mínimo de log
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        handlers=[
+                            logging.StreamHandler(),  # Exibe no terminal
+                            logging.FileHandler('meu_projeto.log', 'w', 'utf-8')  # Salva em arquivo
+                        ])
+
+# Chama a função de configuração de logs
+configurar_logs()
