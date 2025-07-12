c137d37052c58db9c6668a784ac7717219cd9d7b
Matheus Cascalho <cascalhom@gmail.com>
2025-07-12 11:40:10 -0300

    definição da ferrovia após a criação do RailroadEvolutionMemory

--- models/system_evolution_memory.py
+++ models/system_evolution_memory.py
@@ -4,17 +4,34 @@ from models.des_model import DESModel
 from models.railroad import Railroad
 from models.tfr_state_factory import TFRStateFactory, TFRStateSpaceFactory
 
+from logging import critical
 
 class RailroadEvolutionMemory:
-    def __init__(self, railroad: Railroad, memory_size: int) -> None:
+    def __init__(self, railroad: Railroad=None, memory_size: int=1000) -> None:
         self._memory = deque(maxlen=memory_size)
-        self.railroad = railroad
+        self._railroad = railroad
 
-    def take_a_snapshot(self):
+    @property
+    def railroad(self) -> Railroad:
+        return self._railroad
+
+    @railroad.setter
+    def railroad(self, railroad: Railroad) -> None:
+        self._railroad = railroad
+
+    def take_a_snapshot(self, *args, **kwargs):
+        if not self.railroad:
+            critical("Memory does not know the railroad and therefore does not perform any snapshots")
+            return
         state = TFRStateFactory(self.railroad)
         self.memory.append(state)
+
 
     @property
     def memory(self):
         return self._memory
+
+    def __repr__(self):
+        states = len(self.memory)
+        return f"Memory of {states} states of {self.railroad}"
 
