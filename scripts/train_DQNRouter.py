328bbd1fd25d70528b2d1a48221013b54e35a921
Matheus Cascalho <cascalhom@gmail.com>
2025-07-13 09:21:07 -0300

    script de treinamento do DQN

---
+++ scripts/train_DQNRouter.py
@@ -0,0 +1,49 @@
+import sys
+
+# Adicionando o diretório ao sys.path
+sys.path.append('../')
+
+from models.DQNRouter import DQNRouter
+from models.tfr_state_factory import TFRStateSpaceFactory
+from models.system_evolution_memory import RailroadEvolutionMemory
+from models.event import DecoratedEventFactory
+from models.event_calendar import EventCalendar
+from models.des_simulator import DESSimulator
+import dill
+from datetime import datetime, timedelta
+from models.clock import Clock
+from tqdm import tqdm
+from logging import info
+import warnings
+warnings.filterwarnings('ignore')
+N_EPISODES = 10000
+
+def run_episode():
+    simple_clock = Clock(
+        start=datetime(2025, 4, 1),
+        discretization=timedelta(hours=1)
+    )
+    memory = RailroadEvolutionMemory()
+    event_factory = DecoratedEventFactory(
+        pre_method=memory.save_previous_state,
+        pos_method=memory.save_consequence
+    )
+    calendar = EventCalendar(event_factory=event_factory)
+    sim = DESSimulator(clock=simple_clock, calendar=calendar)
+    with open('../tests/artifacts/model_to_train_15.dill', 'rb') as f:
+        model = dill.load(f)
+    state_space = TFRStateSpaceFactory(model)
+    router = DQNRouter(state_space=state_space, demands=model.demands)
+
+    model.router = router
+    memory.railroad = model
+    memory.add_observers([router])
+    with router:
+        sim.simulate(model=model, time_horizon=timedelta(days=60))
+
+    return router.operated_volume(), router.total_demand()
+
+
+for episode in range(N_EPISODES):
+    op_vol, dem = run_episode()
+    info(f'Episode {episode} - Volume: {op_vol} | Demanda: {dem}')
