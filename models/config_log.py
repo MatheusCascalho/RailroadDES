1bcf9560dfd4c0e4b7da898e20f87846d93d8b1e
Matheus Cascalho <cascalhom@gmail.com>
2025-07-13 16:09:35 -0300

    atualização nas configurações de log

--- models/config_log.py
+++ models/config_log.py
@@ -3,11 +3,11 @@ import logging
 
 def configurar_logs():
     # Configuração básica de logging
-    logging.basicConfig(level=logging.INFO,  # Define o nível mínimo de log
+    logging.basicConfig(level=logging.CRITICAL,  # Define o nível mínimo de log
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         handlers=[
                             logging.StreamHandler(),  # Exibe no terminal
-                            logging.FileHandler('meu_projeto.log', 'w', 'utf-8')  # Salva em arquivo
+                            logging.FileHandler('meu_projeto_2.log', 'w', 'utf-8')  # Salva em arquivo
                         ])
 
 # Chama a função de configuração de logs
