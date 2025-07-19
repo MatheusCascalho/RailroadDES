e3421532bb65ea44a3c5069da89c5f72d5b97662
Matheus Cascalho <cascalhom@gmail.com>
2025-07-19 14:39:18 -0300

    remoção de código obsoleto

--- models/railroad_mesh.py
+++ models/railroad_mesh.py
@@ -88,38 +88,11 @@ class RailroadMesh:
         all_points = self.load_points + self.unload_points
         return all_points.__iter__()
 
-    # def transit_time(self, origin_id, destination_id):
-    #     is_loaded_transit = origin_id in [node.identifier for node in self.load_points]
-    #     for transit in self.transit_times:
-    #         if (
-    #                 is_loaded_transit and
-    #                 self.name_to_node[transit.load_origin].identifier == origin_id and
-    #                 self.name_to_node[transit.load_destination].identifier == destination_id
-    #         ):
-    #             return transit.loaded_time
-    #         elif (
-    #                 not is_loaded_transit and
-    #                 self.name_to_node[transit.load_origin].identifier == destination_id and
-    #                 self.name_to_node[transit.load_destination].identifier == origin_id
-    #         ):
-    #             return transit.empty_time
-    #     return timedelta(days=float('inf'))
-
     def node_by_id(self, identifier):
         name = self.id_to_name[identifier]
         node = self.name_to_node[name]
         return node
 
-    # def predicted_time_for_path(self, path: list[int], current_time: datetime):
-    #     total_time = timedelta()
-    #     for origin_id, destination_id in zip(path[:-1], path[1:]):
-    #         destination = self.node_by_id(destination_id)
-    #         transit = self.transit_time(origin_id=origin_id, destination_id=destination_id)
-    #         time_on_destination = destination.predicted_time(current_time=current_time)
-    #         total_time += transit + time_on_destination
-    #     return total_time
-
-    # @staticmethod
     def complete_path(self, origin_name, destination_name):
         # Improvement: Dijkstra algorithm to complex mesh
         return [self.name_to_node[origin_name].identifier, self.name_to_node[destination_name].identifier]
