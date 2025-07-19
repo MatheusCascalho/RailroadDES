e66744160e77ebe59798f16afdb98e648a06e4df
Matheus Cascalho <cascalhom@gmail.com>
2025-07-19 09:25:18 -0300

    módulo de target

--- models/TargetRouter.py
+++ models/TargetRouter.py
@@ -1,41 +1,8 @@
-from collections import defaultdict
-from dataclasses import asdict
+from models.target import TargetManager, SimpleTargetManager
 from models.task import Task
-from models.demand import Demand, Flow
-from models.state_machine import ExpandableStateMachine, Transition, State
-from models.observers import AbstractObserver
+from models.demand import Demand
 from models.router import Router
-from abc import ABC, abstractmethod
-from random import randint
-from interfaces.train_interface import TrainInterface
-from datetime import datetime
-from typing import Any
-import json
 
-class TargetManager(ABC):
-    @abstractmethod
-    def furthest_from_the_target(self):
-        pass
-
-class SimpleTargetManager(TargetManager):
-    def __init__(self, demand: list[Demand]):
-        self.demand = demand
-        self.target = {
-            d.flow: {
-                'demand':d,
-                'target': d.volume
-            } for d in demand
-        }
-
-    def furthest_from_the_target(self):
-        rank = [
-            {
-                'demand': d['demand'],
-                'distance_to_target': d['target'] - d['demand'].promised
-            } for d in self.target.values()
-        ]
-        rank = sorted(rank, key=lambda x: x['distance_to_target'])
-        return rank[0]['demand']
 
 class TargetRouter(Router):
     def __init__(
