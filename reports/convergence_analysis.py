d2b3370b0111ad9a7bd399c23d18203e18ebf008
Matheus Cascalho <cascalhom@gmail.com>
2025-07-23 22:18:06 -0300

    adição de coluna de epsilon final e geração de gráficos auxiliares de análise

--- reports/convergence_analysis.py
+++ reports/convergence_analysis.py
@@ -3,7 +3,7 @@ import plotly.express as px
 
 df = pd.read_csv('logs_treinamento_23072025.log', sep=' - ')
 
-df.columns=['date', 'log_type', 'episode', 'volume', 'demand']
+df.columns=['date', 'log_type', 'episode', 'volume', 'demand', 'epsilon']
 df['date'] = pd.to_datetime(df['date'])
 df['volume'] = df['volume'].apply(lambda x: float(x[len('volume: '):]))
 df['demand'] = df['demand'].apply(lambda x: float(x[len('demanda: '):]))
@@ -11,7 +11,14 @@ df['demand'] = df['demand'].apply(lambda x: float(x[len('demanda: '):]))
 # df.set_index('date', inplace=True)
 # df['volume'].plot(kind='line', color='blue')
 
-fig = px.line(df, x="date", y="volume", title="Convergencia do algoritmo")
+# fig = px.scatter(df, x="date", y="volume", title="Convergencia do algoritmo")
+fig = px.histogram(df, x='volume')
 # fig.update_traces(line_color=color)
 
+fig.show()
+
+fig = px.line(df, x="date", y="volume", title="Convergencia do algoritmo")
+fig.show()
+
+fig = px.scatter(df, x="date", y="volume", title="Convergencia do algoritmo")
 fig.show()
