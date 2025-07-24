202b3417e9e2c5e769665e83fcd878eb0a098189
Matheus Cascalho <cascalhom@gmail.com>
2025-07-24 00:08:13 -0300

    desacomplamento entre tomada de decisão (DQNRouter) e aprendizagem (ActorLearner)

--- models/DQNRouter.py
+++ models/DQNRouter.py
@@ -2,6 +2,7 @@ from interfaces.train_interface import TrainInterface
 from models.TFRState import TFRStateSpace
 from models.observers import AbstractObserver
 from models.router import Router
+from models.system_evolution_memory import RailroadEvolutionMemory
 from datetime import datetime
 from typing import Any
 from models.task import Task
@@ -56,37 +57,36 @@ class ActionSpace:
             return self.sample()
         return self.demands[i]
 
-class DQNRouter(Router, AbstractObserver):
+class ActorLearner(AbstractObserver):
     def __init__(
             self,
-            demands,
             state_space: TFRStateSpace,
+            action_space: ActionSpace,
             policy_net_path: str = '../serialized_models/policy_net.dill',
             target_net_path: str = '../serialized_models/target_net.dill',
             target_update_freq: int = 10,
             epsilon: float = EPSILON_DEFAULT,
-            explortation_method: callable = None,
     ):
-        super().__init__(demands=demands)
-        self.action_space = ActionSpace(demands)
-        AbstractObserver.__init__(self)
+        self.state_space = state_space
+        self.action_space = action_space
         suffix = f"{state_space.cardinality}x{self.action_space.n_actions}_TFRState_v1"
         offset = len('.dill')
+
         policy_net_path = policy_net_path[:-offset] + f"_{suffix}" + policy_net_path[-offset:]
         target_net_path = target_net_path[:-offset] + f"_{suffix}" + target_net_path[-offset:]
-        self.completed_tasks = []
-        self.running_tasks = {}
-        self.state_space = state_space
+
+        self.policy_net = self.load_policies(policy_net_path)
+        self.target_net = self.load_policies(target_net_path)
+
         self.target_update_freq = target_update_freq
         self.policy_net_path = policy_net_path
         self.target_net_path = target_net_path
         self.epsilon = epsilon
-        self.policy_net = self.load_policies(policy_net_path)
-        self.target_net = self.load_policies(target_net_path)
+
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=LEARNING_RATE)
         self.criterion = nn.MSELoss()
         self.episode = 0
-        self.explore = explortation_method if explortation_method else self.action_space.sample
+        super().__init__()
 
     def load_policies(self, filepath: str):
         try:
@@ -98,25 +98,6 @@ class DQNRouter(Router, AbstractObserver):
                 n_actions=self.action_space.n_actions,
             )
         return policy
-
-    def choose_task(self, current_time, train_size, model_state):
-        if self.subjects[0].last_item is None or random.random() < self.epsilon:
-            selected_demand = self.explore()
-        else:
-            with torch.no_grad():
-                state = self.subjects[0].last_item.next_state
-                state = self.state_space.to_array(state)
-                state = torch.FloatTensor(state).unsqueeze(0)
-                demand_index = self.policy_net(state).argmax().item()
-                selected_demand = self.action_space.get_demand(demand_index)
-        task = Task(
-            demand=selected_demand,
-            path=[selected_demand.flow.origin, selected_demand.flow.destination],
-            task_volume=train_size,
-            current_time=current_time,
-            state=model_state
-        )
-        return task
 
     @property
     def memory(self):
@@ -149,7 +130,6 @@ class DQNRouter(Router, AbstractObserver):
         loss.backward()
         self.optimizer.step()
 
-        self.update_epsilon()
 
     def update(self):
         self.episode += 1
@@ -158,11 +138,6 @@ class DQNRouter(Router, AbstractObserver):
 
         if self.episode % self.target_update_freq == 0:
             self.target_net.load_state_dict(self.policy_net.state_dict())
-
-    def update_epsilon(self):
-        # Decaimento do epsilon
-        if self.epsilon > epsilon_min:
-            self.epsilon *= epsilon_decay
 
     def __enter__(self, *args, **kwargs):
         debug(f"Início da aprendizagem do DQNRouter")
@@ -174,3 +149,49 @@ class DQNRouter(Router, AbstractObserver):
 
         with open(self.target_net_path, 'wb') as f:
             dill.dump(self.target_net, f)
+
+
+class DQNRouter(Router):
+    def __init__(
+            self,
+            demands,
+            state_space: TFRStateSpace,
+            policy_net: DQN,
+            simulation_memory: RailroadEvolutionMemory,
+            exploration_method: callable = None,
+            epsilon: float = EPSILON_DEFAULT,
+    ):
+        super().__init__(demands=demands)
+        self.action_space = ActionSpace(demands)
+        self.completed_tasks = []
+        self.running_tasks = {}
+        self.state_space = state_space
+        self.explore = exploration_method if exploration_method else self.action_space.sample
+        self.policy_net = policy_net
+        self.memory = simulation_memory
+        self.epsilon = epsilon
+
+    def choose_task(self, current_time, train_size, model_state):
+        if self.memory.last_item is None or random.random() < self.epsilon:
+            selected_demand = self.explore()
+        else:
+            with torch.no_grad():
+                state = self.memory.last_item.next_state
+                state = self.state_space.to_array(state)
+                state = torch.FloatTensor(state).unsqueeze(0)
+                demand_index = self.policy_net(state).argmax().item()
+                selected_demand = self.action_space.get_demand(demand_index)
+        task = Task(
+            demand=selected_demand,
+            path=[selected_demand.flow.origin, selected_demand.flow.destination],
+            task_volume=train_size,
+            current_time=current_time,
+            state=model_state
+        )
+        self.update_epsilon()
+        return task
+
+    def update_epsilon(self):
+        # Decaimento do epsilon
+        if self.epsilon > epsilon_min:
+            self.epsilon *= epsilon_decay
