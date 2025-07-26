62bc5b107ef1fc28b1da36535f1bf261d31bce98
Matheus Cascalho <cascalhom@gmail.com>
2025-07-26 11:16:09 -0300

    criação de nome e memória persistente em DillQueue

--- models/dill_queue.py
+++ models/dill_queue.py
@@ -2,11 +2,14 @@ import multiprocessing
 import dill
 
 class DillQueue:
-    def __init__(self, maxsize=10_000):
+    def __init__(self, name, maxsize=10_000):
+        self.name = name
         self.queue = multiprocessing.Queue(maxsize=maxsize)
+        self.persistent_memory = []
 
     def put(self, item):
         self.queue.put(dill.dumps(item))
+        self.persistent_memory.append(item)
         pass
 
     def get(self, timeout=1):
