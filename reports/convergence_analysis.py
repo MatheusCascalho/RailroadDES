8bf3140267a58a4940acbd20e9617f72b7593aeb
Matheus Cascalho <cascalhom@gmail.com>
2025-07-26 00:39:10 -0300

    atualização do script de análise de convergência

--- reports/convergence_analysis.py
+++ reports/convergence_analysis.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('logs_treinamento_23072025.log', sep=' - ')
+df = pd.read_csv('treinamento_paralelo.csv', sep=' - ')
 
-df.columns=['date', 'log_type', 'episode', 'volume', 'demand', 'epsilon']
+df.columns=['date', 'log_type','log number', 'episode', 'PID', 'volume', 'demand', 'epsilon']
 df['date'] = pd.to_datetime(df['date'])
 df['volume'] = df['volume'].apply(lambda x: float(x[len('volume: '):]))
 df['demand'] = df['demand'].apply(lambda x: float(x[len('demanda: '):]))
@@ -12,7 +12,7 @@ df['demand'] = df['demand'].apply(lambda x: float(x[len('demanda: '):]))
 # df['volume'].plot(kind='line', color='blue')
 
 # fig = px.scatter(df, x="date", y="volume", title="Convergencia do algoritmo")
-fig = px.histogram(df, x='volume')
+fig = px.histogram(df, x='volume', color='PID')
 # fig.update_traces(line_color=color)
 
 fig.show()
