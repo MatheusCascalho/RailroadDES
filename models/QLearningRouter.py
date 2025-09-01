b135e11980aaaf15ebd7c0171fcec6d04c7444b5
Matheus Cascalho <cascalhom@gmail.com>
2025-09-01 00:49:36 -0300

    função de aprendizagem da Q-Table

--- models/QLearningRouter.py
+++ models/QLearningRouter.py
@@ -17,167 +17,51 @@ import dill
 import random
 import numpy as np
 from logging import debug
-from collections import Counter
+from collections import Counter, defaultdict
+from models.TFRState import TFRState
+from models.action_space import ActionSpace
+from models.system_evolution_memory import Experience, RailroadEvolutionMemory
+
+ALPHA = 0.2
+GAMMA = 0.9
 
 
-EPSILON_DEFAULT = 1.0
-N_NEURONS = 64
-BATCH_SIZE = 50
-GAMMA = 0.99
-LEARNING_RATE = 1e-3
-epsilon_min = 0.01
-epsilon_decay = 0.89
-
-
-class DQN(nn.Module):
-    def __init__(self, n_states, n_actions):
-        super(DQN, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(n_states, N_NEURONS),
-            nn.ReLU(),
-            nn.Linear(N_NEURONS, n_actions),
-        )
-
-    def forward(self, x):
-        return self.fc(x)
-
-class ActionSpace:
-    def __init__(self, demands):
-        self.demands = demands + ['AUTOMATIC', 'ROUTING']
-
-    @property
-    def n_actions(self):
-        return len(self.demands)
-
-    def sample(self):
-        i = random.randint(0, len(self.demands) - 3)
-        return self.demands[i]
-
-    def to_scalar(self, action):
-        flows = [str(d.flow) for d in self.demands[:-2]] + self.demands[-2:]
-        v = flows.index(action)
-        return v
-
-    def get_demand(self, i):
-        if i == len(self.demands) - 1:
-            return self.sample()
-        return self.demands[i]
-
-class Learner:
-    def __init__(
-            self,
-            state_space: TFRStateSpace,
-            action_space: ActionSpace,
-            policy_net_path: str = '../serialized_models/policy_net.dill',
-            target_net_path: str = '../serialized_models/target_net.dill',
-            target_update_freq: int = 10,
-            epsilon: float = EPSILON_DEFAULT,
-    ):
-        self._memory = deque(maxlen=100_000)
-        self.state_space = state_space
+class QTable(AbstractObserver):
+    def __init__(self,  action_space: ActionSpace, learning_rate=ALPHA, discount_factor=GAMMA):
+        self.q_table = defaultdict(lambda : defaultdict(float))
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
         self.action_space = action_space
-        suffix = f"{state_space.cardinality}x{self.action_space.n_actions}_TFRState_v2"
-        offset = len('.dill')
-
-        policy_net_path = policy_net_path[:-offset] + f"_{suffix}" + policy_net_path[-offset:]
-        target_net_path = target_net_path[:-offset] + f"_{suffix}" + target_net_path[-offset:]
-
-        self.policy_net = self.load_policies(policy_net_path)
-        self.target_net = self.load_policies(target_net_path)
-
-        self.target_update_freq = target_update_freq
-        self.policy_net_path = policy_net_path
-        self.target_net_path = target_net_path
-        self.epsilon = epsilon
-
-        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=LEARNING_RATE)
-        self.criterion = nn.MSELoss()
-        self.episode = 0
         super().__init__()
 
-    def load_policies(self, filepath: str):
-        try:
-            with open(filepath, 'rb') as f:
-                policy = dill.load(f)
-        except FileNotFoundError:
-            policy = DQN(
-                n_states=self.state_space.cardinality,
-                n_actions=self.action_space.n_actions,
-            )
-        return policy
+    def learn(self, current_state, next_state: TFRState, action):
+        q_actual = self.q_table.get(current_state, 0).get(action)
+        reward = next_state.reward()
+        max_future_value = max(self.q_table[next_state, a] for a in self.action_space[next_state])
+        q_next = q_actual + self.learning_rate * (reward + self.dicount_factor * max_future_value - q_actual)
+        self.q_table[current_state, action] = q_next
+
+    def update(self):
+        current_state = self.memory.last_item.state
+        next_state = self.memory.last_item.next_state
+        self.learn(
+            current_state=current_state,
+            next_state=next_state,
+            action=self.memory[-1].action
+        )
 
     @property
-    def memory(self):
-        return self._memory
-
-    @property
-    def memory_to_train(self):
-        return [e for e in self._memory if e.action not in ['AUTOMATIC', 'ROUTING'] and not e.state.is_initial]
-
-    def learn(self):
-        # Amostra mini-batch
-        batch = random.sample(self.memory, BATCH_SIZE)
-        states, actions, rewards, next_states, dones = zip(*batch)
-        states = [self.state_space.to_array(s) for s in states]
-        states = torch.FloatTensor(states)
-        actions = [self.action_space.to_scalar(a) for a in actions]
-        actions = torch.LongTensor(actions).unsqueeze(1)
-        rewards = torch.FloatTensor(rewards).unsqueeze(1)
-        next_states = [self.state_space.to_array(s) for s in next_states]
-        next_states = torch.FloatTensor(next_states)
-        dones = torch.FloatTensor(dones).unsqueeze(1)
-
-        # Q-alvo
-        with torch.no_grad():
-            max_next_q = self.target_net(next_states).max(1, keepdim=True)[0]
-            target_q = rewards + GAMMA * max_next_q * (1 - dones)
-
-        # Q previsto
-        current_q = self.policy_net(states).gather(1, actions)
-
-        # Loss e otimização
-        loss = self.criterion(current_q, target_q)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+    def memory(self) -> RailroadEvolutionMemory:
+        return self.subjects[0]
 
 
-    def update(self, experience):
-        self.memory.append(experience)
-        self.episode += 1
-        if len(self.memory_to_train) >= BATCH_SIZE:
-            self.learn()
-
-        if self.episode % self.target_update_freq == 0:
-            self.target_net.load_state_dict(self.policy_net.state_dict())
-
-    def __enter__(self, *args, **kwargs):
-        debug(f"Início da aprendizagem do DQNRouter")
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self.save(*args, **kwargs)
-
-    def save(self, *args, **kwargs):
-        with open(self.policy_net_path, 'wb') as f:
-            dill.dump(self.policy_net, f)
-
-        with open(self.target_net_path, 'wb') as f:
-            dill.dump(self.target_net, f)
-
-        with open(f'current/all_experiences_{os.getpid()}.dill', 'wb') as f:
-            dill.dump(self.memory, f)
-
-
-class DQNRouter(Router):
+class QRouter(Router):
     def __init__(
             self,
             demands,
             state_space: TFRStateSpace,
-            policy_net: DQN,
             simulation_memory: RailroadEvolutionMemory,
             exploration_method: callable = None,
-            epsilon: float = EPSILON_DEFAULT,
     ):
         super().__init__(demands=demands)
         self.action_space = ActionSpace(demands)
