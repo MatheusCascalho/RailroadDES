0d2d3686d14874ea129af22d3db94ab90d4bb277
Matheus Cascalho <cascalhom@gmail.com>
2025-08-31 22:19:54 -0300

    correções na classe de espaço de ações

--- models/DQNRouter.py
+++ models/DQNRouter.py
@@ -47,13 +47,15 @@ class ActionSpace:
 
     @property
     def n_actions(self):
-        return len(self.demands)
+        return len(self.demands) - 2
 
     def sample(self):
         i = random.randint(0, len(self.demands) - 3)
         return self.demands[i]
 
     def to_scalar(self, action):
+        if not isinstance(action, str):
+            action = str(action)
         flows = [str(d.flow) for d in self.demands[:-2]] + self.demands[-2:]
         v = flows.index(action)
         return v
